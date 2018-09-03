72c0b6806130e22187a82e5ac85103b4bbad7063
Daniel Agar <daniel@agar.ca>
2018-09-02 21:24:32 -0400

    WIP: HIL unit tests python

--- Tools/HIL/run_tests.py
+++ Tools/HIL/run_tests.py
@@ -5,45 +5,65 @@ import subprocess
 from subprocess import call, Popen
 from argparse import ArgumentParser
 import re
+import sys
+import unittest
 
-def do_tests(port, baudrate):     
+def do_test(test_cmd, port, baudrate=57600):
+
     databits = serial.EIGHTBITS
     stopbits = serial.STOPBITS_ONE
     parity = serial.PARITY_NONE
     ser = serial.Serial(port, baudrate, databits, parity, stopbits, 100)
     ser.write('\n\n')
     
-    finished = 0
-    while finished == 0:
+    finished = False
+    while not finished:
         serial_line = ser.readline()
         print(serial_line.replace('\n','')) 
 
         if "nsh>" in serial_line:
-            finished = 1
+            finished = True
         time.sleep(0.05)
 
-    ser.write('tests perf\n')
+    ser.write('tests %s\n' % test_cmd)
 
-    finished = 0
-    while finished == 0:
+    finished = False
+    while not finished:
         serial_line = ser.readline()
         print(serial_line.replace('\n','')) 
 
-        if "perf PASSED" in serial_line:
-            finished = 1
+        if "PASSED" in serial_line:
+            finished = True
             ser.close()
 
         time.sleep(0.05)
 
-    ser.close() 
+    ser.close()
 
-def main():
+    return finished
+
+
+class PX4Tests(unittest.TestCase):
+    DEVICE = "/dev/ttyUSB0"
+    BAUDRATE = 57600
+
+    def test_perf(self):
+        self.assertTrue(do_test("perf", self.DEVICE, self.BAUDRATE))
+
+    def test_adc(self):
+        self.assertTrue(do_test("adc", self.DEVICE, self.BAUDRATE))
+ 
+if __name__ == '__main__':
     parser = ArgumentParser(description=__doc__)
-    parser.add_argument('--device', "-d", nargs='?', default = None, help='')
+    parser.add_argument('--device', "-d", nargs='?', default="/dev/ttyUSB0", help='')
     parser.add_argument("--baudrate", "-b", dest="baudrate", type=int, help="Mavlink port baud rate (default=57600)", default=57600)
     args = parser.parse_args()
 
-    do_tests(args.device, args.baudrate)
+    print args
 
-if __name__ == "__main__":
-   main()
+    sys.argv = sys.argv[:1]  
+
+    PX4Tests.DEVICE = args.device
+    PX4Tests.BAUDRATE = args.baudrate
+
+    unittest.main()
